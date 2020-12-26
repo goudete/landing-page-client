c06d04fd6b6224136c5a098f7759a461660b2064
goudete <goudetenrique@gmail.com>
2020-12-26 15:17:52 -0600

    added stripe checkout integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import { Button, FormGroup, InputGroup } from '@blueprintjs/core';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51H39zhHghzEwbJh6ffD4puoh6W3Hq4iiwcojkcOmSADuOCrcytX8KjcbwG3w20G5V7n7vqNoTfyUenEnrboUacAd008RwwuCD3');
+
 
 function App() {
+  const handleClick = async (event) => {
+    // Get Stripe.js instance
+    const stripe = await stripePromise;
+
+    // Call your backend to create the Checkout Session
+    const response = await fetch('/create-checkout-session', { method: 'POST' });
+
+    const session = await response.json();
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -30,6 +56,9 @@ function App() {
                 </Button>
 
             </FormGroup>
+            <button role="link" onClick={handleClick}>
+              Checkout
+            </button>
           </div>
         </div>
       </header>
